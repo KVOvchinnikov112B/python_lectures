e2dc54cfc3955ebb2d0a7e8356a7b283d6927ce4
KVOvchinnikov112B <91244514+KVOvchinnikov112B@users.noreply.github.com>
2021-09-30 19:19:45 +0300

    add
    added 5 pys

---
+++ 1.py
@@ -0,0 +1,11 @@
+print('Введите 2 числа')
+a = input()
+b = input()
+bruh = a
+a = b
+b = bruh
+print('Вывод:')
+print(a)
+print(b)
+
+
---
+++ 3.py
@@ -0,0 +1,6 @@
+print('введите 2 числа')
+a = int(input())
+b = int(input())
+pascal = 1
+pascal = (b * 1000) + a
+print(pascal)
