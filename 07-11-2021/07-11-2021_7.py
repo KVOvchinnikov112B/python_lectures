06f813b97a888c80d75c4e6ffe4b7e77490a0ee0
KVOvchinnikov112B <91244514+KVOvchinnikov112B@users.noreply.github.com>
2021-10-13 19:42:32 +0300

    added 6 and 7

---
+++ 07-11-2021/07-11-2021_7.py
@@ -0,0 +1,14 @@
+print ('Введите начальную клетку')
+str1 = int(input())
+stol1 = int(input())
+print ('введите конечную клетку')
+str2 = int(input())
+stol2 = int(input())
+
+if ( (str1-1 == str2) or (str1+1 == str2) ) and ( (stol1+2 == stol2) or (stol1-2 == stol2) ) or( (stol1-1 == stol2) or (stol1+1 == stol2) ) and ( (str1+2 == str2) or (str1-2 == str2) ) :
+     print('YES')
+else:
+    print ('NO')
+
+    
+ 
