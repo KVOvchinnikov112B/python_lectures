360d1a1a17102b280b58d26529da55f1cc5134c3
Константин Овчинников <91244514+KVOvchinnikov112B@users.noreply.github.com>
2022-05-13 16:31:10 +0300

    fixed

--- 2nd_term/classes.py
+++ 2nd_term/classes.py
@@ -11,35 +11,40 @@ class matrix:
         for i in range(self.h*self.w):
             self.A.append( [round((random.random()),1),round((random.random()),1),round((random.random()),1)] )
 
+    def write(self):
         for i in range(1,self.h*self.w+1):
             print (self.A[i-1], end=' ')
             if ((i) % self.w == 0):
                 print('')
 
     def grayscale(self):
-        B = []
+        self.B = []
         for i in range(self.h*self.w):
-            B.append( round(sum(self.A[i])/len(self.A[i]),1) )
+            self.B.append( round(sum(self.A[i])/len(self.A[i]),1) )
 
+    def grayscale_print(self):
         for i in range(1,self.h*self.w+1):
-            print (B[i-1], end=' ')
+            print (self.B[i-1], end=' ')
             if ((i) % self.w == 0):
                 print('')
 
     def negative(self):
-         C = []
+         self.C = []
          for i in range(self.h*self.w):
-             C.append( (round(1-self.A[i][0],1),round(1-self.A[i][1],1),round(1-self.A[i][2],1))  )
+             self.C.append( (round(1-self.A[i][0],1),round(1-self.A[i][1],1),round(1-self.A[i][2],1))  )
 
+    def negative_print(self):
          for i in range(1,self.h*self.w+1):
-            print (C[i-1], end=' ')
+            print (self.C[i-1], end=' ')
             if ((i) % self.w == 0):
                 print('') 
  
 tom = matrix(12, 4)
 tom.create()
+tom.write()
 print(' ')
 tom.grayscale()
 tom.negative()
 print(' ')
-
+tom.grayscale_print()
+tom.negative_print()
